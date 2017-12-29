cf15654095d3d10ad4b81dec2bbfdc5d9bc9b0e8
xxx <x@x.x>
2017-12-29 23:26:35 +0800

    更新默认时区
    Signed-off-by: xxx <x@x.x>

--- src/main/java/com/example/ShadowSocksShare/Application.java
+++ src/main/java/com/example/ShadowSocksShare/Application.java
@@ -4,9 +4,20 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class Application {
+
+	/**
+	 * 设置默认时区
+	 */
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("GMT+8"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
